4290d8b2f3efb205848abd5421f3679d75bb35f2
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-01-14 19:26:12 +0500

    coding start

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // var doc = document.getElementById("div1").childNodes
 // console.log(doc)
 
-function wordchecker() {
-    var input = document.getElementById("input").value;
-    var words = input.split(" ").reverse().join(" ");
-    document.getElementById("result").innerText = words;
-    console.log(words);
+// function wordchecker() {
+//     var input = document.getElementById("input").value;
+//     var words = input.split(" ").reverse().join(" ");
+//     document.getElementById("result").innerText = words;
+//     console.log(words);
 
-}
+// }
